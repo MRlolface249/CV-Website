61f49d28651c2b54c803526092c3c82d65b0fe7c
MRlolface249 <ahop089@aucklanduni.ac.nz>
2025-07-30 21:12:51 +1200

    added date objects

--- src/app/constants/index.tsx
+++ src/app/constants/index.tsx
@@ -18,24 +18,24 @@ interface Contact {
 interface Education {
   institution: string;
   qualification: string;
-  startYear: string;
-  endYear: string;
+  startYear: Date;
+  endYear: Date;
 }
 
 interface Experience {
   title: string;
   company: string;
   location?: string;
-  startDate?: string;
-  endDate?: string;
+  startDate?: Date;
+  endDate?: Date;
   description: string[];
 }
 
 interface Project {
   title: string;
   link?: string;
-  startDate?: string;
-  endDate?: string;
+  startDate?: Date;
+  endDate?: Date;
   description: string[];
 }
 
@@ -67,14 +67,14 @@ export const education: Education[] = [
   {
     institution: "University of Auckland",
     qualification: "Bachelor of Engineering (Honours) in Software Engineering",
-    startYear: "2021",
-    endYear: "2024",
+    startYear: new Date(2021),
+    endYear: new Date(2024),
   },
   {
     institution: "Avondale College",
     qualification: "Year 13 Cambridge A Levels",
-    startYear: "2016",
-    endYear: "2020",
+    startYear: new Date(2016),
+    endYear: new Date(2020),
   },
 ];
 
@@ -83,8 +83,8 @@ export const work: Experience[] = [
     title: "Research Assistant",
     company: "University of Auckland",
     location: "Auckland, CBD",
-    startDate: "04/2024",
-    endDate: "06/2024",
+    startDate: new Date(2024, 3),
+    endDate: new Date(2024, 5),
     description: [
       "Collaborated on the ENGclusion longitudinal study, following 200 engineering graduates into their careers to increase inclusive outcomes for students.",
       "Streamlined survey forms using JavaScript to help self-populate over 30 data entries for participants.",
@@ -94,8 +94,8 @@ export const work: Experience[] = [
     title: "Software Engineer Intern",
     company: "Alstef Group",
     location: "Auckland, Rosebank",
-    startDate: "11/2023",
-    endDate: "02/2024",
+    startDate: new Date(2023,10),
+    endDate: new Date(2024,1),
     description: [
       "Contributed to the maintenance system for the Swiftdrop self-service bag drop, which processes 100s of millions of international passengers annually.",
       "Restructured the maintenance page by developing a 3D diagnostic page to provide intuitive information to staff.",
@@ -107,8 +107,8 @@ export const work: Experience[] = [
     title: "Retail Assistant, Home Department",
     company: "Smith and Caughey's",
     location: "Auckland, Newmarket",
-    startDate: "04/2023",
-    endDate: "11/2023",
+    startDate: new Date(2023,3),
+    endDate: new Date(2023,10),
     description: [
       "Oversaw the home department and all sales in the department.",
       "Assisted customers with any questions about products in the home department.",
@@ -119,8 +119,8 @@ export const work: Experience[] = [
     title: "Bartender and Waiter",
     company: "Monsoon Poon",
     location: "Auckland, Viaduct",
-    startDate: "01/2022",
-    endDate: "12/2022",
+    startDate: new Date(2022,0),
+    endDate: new Date(2022,11),
     description: [
       "Coordinated communication between floor and kitchen staff to ensure smooth operations.",
       "Worked closely with a team of 3 bartenders to prepare over 100 drinks per night.",
@@ -134,8 +134,8 @@ export const projects: Project[] = [
   {
     title: "Superfluous",
     link: "https://github.com/SuperfluousNZ",
-    startDate: "11/2024",
-    endDate: "Present",
+    startDate: new Date(2024,10),
+    endDate: new Date(),
     description: [
       "Designed a web application using React and TypeScript to help users identify prerequisite media for movies, TV shows, and books/comics.",
       "Engineered a scalable and secure back-end REST API using ASP.NET and Entity Framework to manage media data and relationships.",
@@ -144,8 +144,8 @@ export const projects: Project[] = [
   {
     title: "WIE Scholarship Finder",
     link: "https://wie-scholarship-search.vercel.app/",
-    startDate: "02/2024",
-    endDate: "05/2024",
+    startDate: new Date(2024,1),
+    endDate: new Date(2024,4),
     description: [
       "Partnered with Women in Engineering to develop a scholarship finder application for their official website.",
       "Constructed the back-end using a Next.js API, connected to Neon Serverless PostgreSQL for data management.",
@@ -155,8 +155,8 @@ export const projects: Project[] = [
   {
     title: "Plunket Smart-Scale Server",
     link: "https://github.com/MRlolface249/Capstone",
-    startDate: "02/2024",
-    endDate: "05/2024",
+    startDate: new Date(2024,1),
+    endDate: new Date(2024,4),
     description: [
       "Created a C# REST API to support a website for tracking baby health data.",
       "Designed an SQLite database using a code-first approach with .NET Entity Framework.",
@@ -166,8 +166,8 @@ export const projects: Project[] = [
   {
     title: "Club Membership Manager",
     link: "https://github.com/UOA-CS732-SE750-Students-2024/project-group-blue-cows",
-    startDate: "02/2024",
-    endDate: "05/2024",
+    startDate: new Date(2024,1),
+    endDate: new Date(2024,4),
     description: [
       "Developed a streamlined membership application platform for university clubs using React and TypeScript.",
       "Built the back-end with Next.js using server actions, integrated with a Neon database using Drizzle ORM for efficient data storage and management.",
@@ -177,8 +177,8 @@ export const projects: Project[] = [
   {
     title: "Younite Website",
     link: "https://github.com/UoaWDCC/younite-web",
-    startDate: "03/2023",
-    endDate: "10/2023",
+    startDate: new Date(2023,2),
+    endDate: new Date(2023,9),
     description: [
       "Implemented a display and landing website in 2-week sprints in a group of 8.",
       "Built a dynamic website with customizable project pages using React components.",
@@ -191,8 +191,8 @@ export const volunteering: Experience[] = [
   {
     title: "Social Events Director",
     company: "Software Engineering Student Society (SESA)",
-    startDate: "10/2023",
-    endDate: "10/2024",
+    startDate: new Date(2023,9),
+    endDate: new Date(2024,9),
     description: [
       "Supervised a team to plan, execute, and oversee over 10 social activities for SESA.",
       "Organized all social activities for the 48-hour hackathon attended by 90 participants in collaboration with the Web Development and Consulting Club social lead.",
